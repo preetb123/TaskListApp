3158f704f6e436bdd158312b430f89bc7c0d53db
Preetam <preetb123@gmail.com>
2021-04-10 11:44:09 +0530

    setup e2e test with detox

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['detox'],
+  overrides: [
+    {
+      files: ['*.e2e.ts'],
+      env: {
+        'detox/detox': true,
+        jest: true,
+        'jest/globals': true,
+      },
+    },
+  ],
 };
